f7d66fd445c6f5d9cccf0ba73934de2eaaaf4fa7
haikal036 <ahmadaryan40@gmail.com>
2024-05-11 23:32:40 +0800

    Update evaluate.js

--- evaluate.js
+++ evaluate.js
@@ -1,9 +1,9 @@
-function add() {
+function calculate() {
     var current_plan = parseFloat(document.getElementById('current_plan').value);
     var current_used = parseFloat(document.getElementById('current_used').value);
     var next_plan = parseFloat(document.getElementById('next_plan').value);
     var next_day = parseFloat(document.getElementById('next_day').value);
 
     var upgrade_price = (next_day * next_plan) - (current_used * current_plan);
-    document.getElementById('upgrade_price').textContent = "Result: " + upgrade_price;
+    document.getElementById('result').textContent = "Upgrade Price: " + upgrade_price;
 }
